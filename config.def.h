83c317c37f5f38f0ea8b4afc002c0769a5b22a25
Joysilk <info@joysilk.fr>
2025-07-11 17:46:48 +0200

    Restore window border and remove redundant font styles

--- config.def.h
+++ config.def.h
@@ -3,13 +3,13 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Outfit:style=Regular:size=12",
-                                        "JetBrainsMono Nerd Font:style=Regular:size=12" };
-static const char dmenufont[]       = "Outfit:style=Regular:size=12";
+static const char *fonts[]          = { "Outfit:size=12",
+                                        "JetBrainsMono Nerd Font:size=12" };
+static const char dmenufont[]       = "Outfit:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
