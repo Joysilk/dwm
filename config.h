0316aa2cf77f3afe012dee1c66a648248231cb3f
Joysilk <info@joysilk.fr>
2025-07-13 16:16:53 +0200

    Adapt color scheme to Gruvbox and remove dmenu overrides

--- config.h
+++ config.h
@@ -11,15 +11,14 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Outfit:size=12",
                                         "JetBrainsMono Nerd Font:size=12" };
 static const char dmenufont[]       = "Outfit:size=16";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#1d2021";
+static const char col_gray2[]       = "#3c3836";
+static const char col_gray3[]       = "#a89984";
+static const char col_white[]       = "#ebdbb2";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_white, col_gray2, col_gray3 },
 };
 
 /* tagging */
@@ -61,7 +60,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const char *upvol[]    = { "/bin/sh", "-c", "/usr/bin/wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ && ~/src/scripts/statusbar.sh", NULL };
